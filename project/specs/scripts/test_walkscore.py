165c93a3677b0c95b9520eb5cf14fbb27d4efd4e
Mahmoud Nassif, mabounassif <mahmoud.abounassif@gmail.com>
2016-10-04 21:11:28 -0400

    Fix project

---
+++ project/specs/scripts/test_walkscore.py
@@ -0,0 +1,9 @@
+import unittest
+import walkscore
+
+class TestWalkscore(unittest.TestCase):
+    def test_walkscore_fetch_on_every_listing(self):
+
+
+    def test_should_work(self):
+        self.assertTrue(True)
